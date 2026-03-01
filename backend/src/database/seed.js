e423c582cacca85ee30f1e5eaaac2cab8dfbe04e
Cedar <cedar794@gmail.com>
2026-03-01 17:43:42 +0800

    fix: 修复seed.js数据库路径错误

--- backend/src/database/seed.js
+++ backend/src/database/seed.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join(__dirname, '../../../data/database.db');
+const dbPath = path.join(__dirname, '../../data/database.db');
 const db = new sqlite3.Database(dbPath);
 
 // 社群测试数据
